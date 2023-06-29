338e60a2f6672b0635caa91c4e0f6899bf985c5e
Jean-Michel Gorius <jean-michel.gorius@protonmail.com>
2023-06-29 13:21:09 +0200

    Rename ArithmeticDialect -> ArithDialect

--- standalone-opt/standalone-opt.cpp
+++ standalone-opt/standalone-opt.cpp
@@ -29,7 +29,7 @@ int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
   registry.insert<mlir::standalone::StandaloneDialect>();
   registry.insert<mlir::func::FuncDialect>();
-  registry.insert<mlir::arith::ArithmeticDialect>();
+  registry.insert<mlir::arith::ArithDialect>();
   // Add the following to include *all* MLIR Core dialects, or selectively
   // include what you need like above. You only need to register dialects that
   // will be *parsed* by the tool, not the one generated
